0783d5d1ec1f5d7dfa13f5ca7fdb1b357eca70b7
Poulpy <paul.repain@yahoo.com>
2021-01-27 11:14:56 +0100

    Add topology + finish script

--- script.sh
+++ script.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-#cpupower -c 3 frequency-set --governor performance
-# cpupower -c 3 frequency-set --freq
-cd copy/ && make && taskset -c 3 ./copy_SSE_AVX 24576 1000 | cut -d';' -f1,9 > copy_L1.dat
-cd -
-cd dotprod/ && make && taskset -c 3 ./dotprod_SSE_AVX 24576 1000 | cut -d';' -f1,9 > dotprod_L1.dat
-cd -
-cd load/ && make && taskset -c 3 ./load_SSE_AVX 24576 1000 | cut -d';' -f1,9 > dotprod_L1.dat
-cd -
-cd ntstore/ && make && taskset -c 3 ./ntstore_SSE_AVX 24576 1000 | cut -d';' -f1,9 > ntstore_L1.dat
-cd -
-cd reduc/ && make && taskset -c 3 ./reduc_SSE_AVX 24576 1000 | cut -d';' -f1,9 > reduc_L1.dat
-cd -
-cd store/ && make && taskset -c 3 ./store_SSE_AVX 24576 1000 | cut -d';' -f1,9 > store_L1.dat
-cd -
-cd triad/ && make && taskset -c 3 ./triad_SSE_AVX 24576 1000 | cut -d';' -f1,9 > triad_L1.dat
 
-# gnuplot -c "load_plot.gp" > load_bw.png
+CORE_ID='3'
+BENCHMARKS=(copy dotprod load memcpy ntstore pc reduc store triad)
+CACHES=(L1 L2)
+CACHES_SIZES=(24576 1048576)
+REPETITIONS=1000
+FREQUENCY='1.0GHz'
+
+echo "sudo cpupower -c 3 frequency-set --governor performance"
+#echo "cpupower -c $CORE_ID frequency-set -f $FREQUENCY"
+
+
+for cache in $(seq 0 1)
+do
+    for benchmark in "${BENCHMARKS[@]}"
+    do
+        echo "cd ${benchmark}/"
+        echo "make"
+        echo "taskset -c $CORE_ID ./${benchmark}_${CACHES[$cache]}_SSE_AVX ${CACHES_SIZES[$cache]} $REPETITIONS | cut -d';' -f1,9 > ${benchmark}_${CACHES[$cache]}.dat"
+        echo "cd -"
+    done
+done
+
+
